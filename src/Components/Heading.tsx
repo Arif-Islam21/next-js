7b97434532427d82d66b3505095bb9fcb3b185c9
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-11-04 16:08:21 +0600

    looking for short cut

---
+++ src/Components/Heading.tsx
@@ -0,0 +1,5 @@
+const Heading = () => {
+  return <div>This is my Head</div>;
+};
+
+export default Heading;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Heading from "@/Components/Heading";
+
 export default function Home() {
   return (
     <div>
       <h2>I am Arif</h2>
       <button>Hello BTN</button>
+      <Heading />
     </div>
   );
 }
