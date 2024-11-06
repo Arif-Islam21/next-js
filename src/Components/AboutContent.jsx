ae3bb0ac5d4361d821d10e32692344cea6641136
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-11-06 15:20:08 +0600

    dynamic routeing

--- src/Components/AboutContent.jsx
+++ src/Components/AboutContent.jsx
@@ -3,9 +3,9 @@ import React from "react";
 
 export default function AboutContent() {
   return (
-    <div>
-      <Link href="/history">History</Link>
-      <Link href="/mission">Mission</Link>
+    <div className="flex flex-col space-y-3">
+      <Link href="/about/history">History</Link>
+      <Link href="/about/mission">Mission</Link>
     </div>
   );
 }
