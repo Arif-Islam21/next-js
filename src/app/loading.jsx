aa49b027ba8c837783cca8dfc6d6fa71f8d77b9a
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-11-07 10:26:10 +0600

    loading state added

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+export default function commonLoading() {
+  return (
+    <div>
+      <div className={"text-3xl font-bold text-center text-red-700"}>
+        common Loading ....
+      </div>
+    </div>
+  );
+}
